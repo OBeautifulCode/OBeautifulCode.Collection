8c1d9fa6632284c31fd8ec36d5b2a8bf75b199c3
Suraj Gupta <surajg@gmail.com>
2016-05-13 14:09:11 -0400

    fix nuget break

--- OBeautifulCode.Collection/Properties/AssemblyInfo.cs
+++ OBeautifulCode.Collection/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OBeautifulCode.Collection")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OBeautifulCode.Collection")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OBeautifulCode")]
 [assembly: AssemblyProduct("OBeautifulCode.Collection")]
