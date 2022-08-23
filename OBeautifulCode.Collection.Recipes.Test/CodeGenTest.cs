1b9470bc9ebca600e9416c59d472269770aa8fea
Suraj Gupta <SurajGupta@users.noreply.github.com>
2022-08-23 15:15:45 -0400

    adding GroupingExtensions recipe with ConditionalGroupBy methods

---
+++ OBeautifulCode.Collection.Recipes.Test/CodeGenTest.cs
@@ -0,0 +1,114 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CodeGenTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Collection.Recipes.Test
+{
+    using System.Collections.Generic;
+    using System.Text;
+    using OBeautifulCode.Math.Recipes;
+    using Xunit;
+    using Xunit.Abstractions;
+    using static System.FormattableString;
+
+    public class CodeGenTest
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public CodeGenTest(
+            ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact(Skip = "Generate code only.")]
+        public void GenerateConditionalGroupByCode()
+        {
+            var truthTable = MathHelper.GenerateTruthTable(8, false);
+            var tokenPrefix = "selector";
+            var intToNameMap = new Dictionary<int, string>
+                               {
+                                   { 1, "One" },
+                                   { 2, "Two" },
+                                   { 3, "Three" },
+                                   { 4, "Four" },
+                                   { 5, "Five" },
+                                   { 6, "Six" },
+                                   { 7, "Seven" },
+                                   { 8, "Eight" },
+                               };
+            var result = new StringBuilder();
+            result.AppendLine("IEnumerable<Grouping<IReadOnlyList<object>, TItem>> result;");
+            foreach (var row in truthTable)
+            {
+                /*
+                    // This will print the truth table:
+                    var rowString = row.Select(_ => _ ? "1":"0").ToDelimitedString(",");
+                    this.testOutputHelper.WriteLine(rowString);
+                */
+                result.Append("else if (");
+                for (int idx = 1;
+                    idx <= row.Count;
+                    idx++)
+                {
+                    result.Append(Invariant($"({tokenPrefix}{intToNameMap[idx]}"));
+
+                    if (row[idx - 1])
+                    {
+                        result.Append(" != null)");
+                    }
+                    else
+                    {
+                        result.Append(" == null)");
+                    }
+
+                    if (idx != row.Count)
+                    {
+                        result.Append(" && ");
+                    }
+                }
+
+                result.AppendLine(")");
+                result.AppendLine("{");
+                result.AppendLine("result = values");
+                result.Append(".GroupBy(_ => new");
+                result.Append(" { ");
+                for (int idx = 1;
+                    idx <= row.Count;
+                    idx++)
+                {
+                    if (row[idx - 1])
+                    {
+                        var stringNumber = intToNameMap[idx];
+                        result.Append(Invariant($"Key{stringNumber} = {tokenPrefix}{stringNumber}(_), "));
+                    }
+                }
+
+                result.Append("}");
+                result.AppendLine(")");
+                result.Append(".Select(_ => new Grouping<IReadOnlyList<object>, TItem>(");
+                result.Append("new object[]");
+                result.Append(" {");
+                for (int idx = 1;
+                    idx <= row.Count;
+                    idx++)
+                {
+                    if (row[idx - 1])
+                    {
+                        var stringNumber = intToNameMap[idx];
+                        result.Append(Invariant($"_.Key.Key{stringNumber},"));
+                    }
+                }
+
+                result.Append("}");
+                result.AppendLine(", _))");
+                result.AppendLine(".ToList();");
+                result.AppendLine("}");
+            }
+
+            this.testOutputHelper.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ OBeautifulCode.Collection.Recipes.Test/GroupingExtensionsTest.cs
@@ -0,0 +1,298 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GroupingExtensionsTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Collection.Recipes.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentAssertions;
+    using OBeautifulCode.Assertion.Recipes;
+    using Xunit;
+
+    public static class GroupingExtensionsTest
+    {
+        [Fact]
+        public static void ConditionalGroupBy_TKeyOne___Should_throw_ArgumentNullException___When_parameter_values_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ((IEnumerable<string>)null).ConditionalGroupBy(_ => _));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("values");
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyTwo___Should_throw_ArgumentNullException___When_parameter_values_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ((IEnumerable<string>)null).ConditionalGroupBy(_ => _, _ => _));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("values");
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyThree___Should_throw_ArgumentNullException___When_parameter_values_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ((IEnumerable<string>)null).ConditionalGroupBy(_ => _, _ => _, _ => _));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("values");
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyFour___Should_throw_ArgumentNullException___When_parameter_values_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ((IEnumerable<string>)null).ConditionalGroupBy(_ => _, _ => _, _ => _, _ => _));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("values");
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyFive___Should_throw_ArgumentNullException___When_parameter_values_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ((IEnumerable<string>)null).ConditionalGroupBy(_ => _, _ => _, _ => _, _ => _, _ => _));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("values");
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeySix___Should_throw_ArgumentNullException___When_parameter_values_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ((IEnumerable<string>)null).ConditionalGroupBy(_ => _, _ => _, _ => _, _ => _, _ => _, _ => _));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("values");
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeySeven___Should_throw_ArgumentNullException___When_parameter_values_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ((IEnumerable<string>)null).ConditionalGroupBy(_ => _, _ => _, _ => _, _ => _, _ => _, _ => _, _ => _));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("values");
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyEight___Should_throw_ArgumentNullException___When_parameter_values_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => ((IEnumerable<string>)null).ConditionalGroupBy(_ => _, _ => _, _ => _, _ => _, _ => _, _ => _, _ => _, _ => _));
+
+            // Assert
+            actual.AsTest().Must().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("values");
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyOne___Should_return_single_group_with_all_elements_of_parameter_value___When_all_selectors_are_null()
+        {
+            // Arrange
+            var inputs = new[]
+            {
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            Func<Tuple<string, string, string>, string> nullFunc = null;
+            var groups = inputs.ConditionalGroupBy(nullFunc);
+
+            // Assert
+            groups.Count().MustForTest().BeEqualTo(1);
+            groups.Single().ToList().MustForTest().BeEqualTo(inputs.ToList());
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyTwo___Should_return_single_group_with_all_elements_of_parameter_value___When_all_selectors_are_null()
+        {
+            // Arrange
+            var inputs = new[]
+            {
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            Func<Tuple<string, string, string>, string> nullFunc = null;
+            var groups = inputs.ConditionalGroupBy(nullFunc, nullFunc);
+
+            // Assert
+            groups.Count().MustForTest().BeEqualTo(1);
+            groups.Single().ToList().MustForTest().BeEqualTo(inputs.ToList());
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyThree___Should_return_single_group_with_all_elements_of_parameter_value___When_all_selectors_are_null()
+        {
+            // Arrange
+            var inputs = new[]
+            {
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            Func<Tuple<string, string, string>, string> nullFunc = null;
+            var groups = inputs.ConditionalGroupBy(nullFunc, nullFunc, nullFunc);
+
+            // Assert
+            groups.Count().MustForTest().BeEqualTo(1);
+            groups.Single().ToList().MustForTest().BeEqualTo(inputs.ToList());
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyFour___Should_return_single_group_with_all_elements_of_parameter_value___When_all_selectors_are_null()
+        {
+            // Arrange
+            var inputs = new[]
+            {
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            Func<Tuple<string, string, string>, string> nullFunc = null;
+            var groups = inputs.ConditionalGroupBy(nullFunc, nullFunc, nullFunc, nullFunc);
+
+            // Assert
+            groups.Count().MustForTest().BeEqualTo(1);
+            groups.Single().ToList().MustForTest().BeEqualTo(inputs.ToList());
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyFive___Should_return_single_group_with_all_elements_of_parameter_value___When_all_selectors_are_null()
+        {
+            // Arrange
+            var inputs = new[]
+            {
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            Func<Tuple<string, string, string>, string> nullFunc = null;
+            var groups = inputs.ConditionalGroupBy(nullFunc, nullFunc, nullFunc, nullFunc, nullFunc);
+
+            // Assert
+            groups.Count().MustForTest().BeEqualTo(1);
+            groups.Single().ToList().MustForTest().BeEqualTo(inputs.ToList());
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeySix___Should_return_single_group_with_all_elements_of_parameter_value___When_all_selectors_are_null()
+        {
+            // Arrange
+            var inputs = new[]
+            {
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            Func<Tuple<string, string, string>, string> nullFunc = null;
+            var groups = inputs.ConditionalGroupBy(nullFunc, nullFunc, nullFunc, nullFunc, nullFunc, nullFunc);
+
+            // Assert
+            groups.Count().MustForTest().BeEqualTo(1);
+            groups.Single().ToList().MustForTest().BeEqualTo(inputs.ToList());
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeySeven___Should_return_single_group_with_all_elements_of_parameter_value___When_all_selectors_are_null()
+        {
+            // Arrange
+            var inputs = new[]
+            {
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            Func<Tuple<string, string, string>, string> nullFunc = null;
+            var groups = inputs.ConditionalGroupBy(nullFunc, nullFunc, nullFunc, nullFunc, nullFunc, nullFunc, nullFunc);
+
+            // Assert
+            groups.Count().MustForTest().BeEqualTo(1);
+            groups.Single().ToList().MustForTest().BeEqualTo(inputs.ToList());
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyEight___Should_return_single_group_with_all_elements_of_parameter_value___When_all_selectors_are_null()
+        {
+            // Arrange
+            var inputs = new[]
+            {
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            Func<Tuple<string, string, string>, string> nullFunc = null;
+            var groups = inputs.ConditionalGroupBy(nullFunc, nullFunc, nullFunc, nullFunc, nullFunc, nullFunc, nullFunc, nullFunc);
+
+            // Assert
+            groups.Count().MustForTest().BeEqualTo(1);
+            groups.Single().ToList().MustForTest().BeEqualTo(inputs.ToList());
+        }
+
+        [Fact]
+        public static void ConditionalGroupBy_TKeyOne___Should_return_expected_groups___When_selectors_are_defined()
+        {
+            // Arrange
+            var group1Key = Guid.NewGuid().ToString();
+
+            var group2Key = Guid.NewGuid().ToString();
+
+            var inputs = new[]
+            {
+                Tuple.Create(group1Key, Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(group2Key, Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(group1Key, Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+                Tuple.Create(group2Key, Guid.NewGuid().ToString(), Guid.NewGuid().ToString()),
+            };
+
+            // Act
+            var groups = inputs.ConditionalGroupBy(_ => _.Item1);
+
+            // Assert
+            groups.AsTest().Must().HaveCount(2);
+
+            foreach (var group in groups)
+            {
+                group.ToList().AsTest().Must().HaveCount(2);
+
+                var expected = inputs.Where(_ => _.Item1 == group.Key.Single().ToString()).ToList();
+
+                group.ToList().AsTest().Must().BeEqualTo(expected);
+            }
+        }
+    }
+}
